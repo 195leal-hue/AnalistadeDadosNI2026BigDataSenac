e6b1cbee483d19f97506c7eb6bcc0f5e87e16352
LUCAS LEAL GUEDES LIMA <lucas.llima@aluno.senac.rio>
2026-08-17 21:09:51 -0300

    teste

---
+++ UC1/Aula100826.py
@@ -0,0 +1,49 @@
+print("Olá,mundo")
+
+# nome = "Maria"
+# idade = 30
+# preco = 19.99
+# esta_matriculda = True
+# notas = [8.0, 7.5]
+# aluno = ("Maria", 30)
+# disciplina = {"Python", "Logica"}
+# cadastro = {"nome": "Maria", "idade": 30}
+
+# print(type(nome))
+# print(type(idade))
+# print(type(preco))
+
+# nota1 = 2
+# nota2 = 4
+
+# x = 15
+# y = 20
+# print("x é maior que o y?", x>y)
+# print("x é igual a y?", x==y)
+# resposta = x=y
+# print(resposta)
+# print(type)
+
+# cnh = True
+# bebidinha = False
+
+# posso_dirigir = cnh and not bebidinha
+# print(posso_dirigir)
+
+busaum = False
+trenzin = False
+
+venho_para_aula = busaum or trenzin
+print("venho para aula?", venho_para_aula)
+
+locomocao = input("diga qual a sua locomoçâo?")
+choveu = True
+if choveu and locomocao=="moto":
+    resultado = "to todo molhado :("
+
+elif not choveu and locomocao =="moto":
+
+
+else:            resultado = "to seco :)"
+
+print(resultado)
