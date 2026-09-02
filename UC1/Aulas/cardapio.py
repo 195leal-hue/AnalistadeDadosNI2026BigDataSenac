868eece69eeaac0b9a81a505c1ee0b37342d6f2a
LUCAS LEAL GUEDES LIMA <lucas.llima@aluno.senac.rio>
2026-09-02 19:00:59 -0300

    Projeto Soma

---
+++ UC1/Aulas/cardapio.py
@@ -0,0 +1,9 @@
+import CARDAPIO
+
+def CARDAPIO = {
+        1: {"nome": "X-Burger Especial", "preco": 28.90},
+        2: {"nome": "Batata Frita Grande", "preco": 22.00},
+        3: {"nome": "Refrigerante Lata", "preco": 6.00},
+        4: {"nome": "Água Mineral", "preco": 4.50},
+        5: {"nome": "Sobremesa do Dia", "preco": 12.00}
+    }
